3576a430bfbd8eaab0a9a2007f31c769c3aa7d3b
sherly-lin <112499134+sherly-lin@users.noreply.github.com>
2023-02-11 22:48:11 -0500

    attempt

--- script.js
+++ script.js
@@ -50,3 +50,11 @@ prevNextIcon.forEach(icon => { // getting prev and next icons
         renderCalendar(); // calling renderCalendar function
     });
 });
+
+
+function showDetail(){
+            
+}
+function hideDetail(){
+    
+}
